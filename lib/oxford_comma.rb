051635b05cbbba1b8a0fff20ff0679094c949d9c
tamminen.mikkoj <tamminen.mikkoj@gmail.com>
2018-10-01 04:47:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length < 2
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    string = ", and " + array.pop
+    string = array.join(", ") << string
+  end
 end
